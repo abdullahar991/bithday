009be32d6f447c2e411849680894d9724d789519
Abdullahar990 <112599265+Abdullahar990@users.noreply.github.com>
2025-05-10 16:20:24 +0500

    Delete Desktop/my-app directory

--- Desktop/my-app/src/app/api/doctors/list.js
+++
@@ -1,7 +0,0 @@
-import {db} from '../../../../lib/db'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-    const [rows] = await db.execute('SELECT * FROM doctors WHERE available = 1')
-    return NextResponse.json(rows)
-}
