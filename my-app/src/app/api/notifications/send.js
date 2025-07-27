7ceaabf6d9d048c773013deee9d0f50f9e114939
abdullahar991 <abdullahar991@gmail.com>
2025-07-27 10:43:58 +0500

    Delete my-app directory

--- my-app/src/app/api/notifications/send.js
+++
@@ -1,8 +0,0 @@
-import {db} from '../../../../lib/db'
-import { NextResponse } from 'next/server'
-
-export async function POST(req) {
-    const { user_id, message, type } = await req.json()
-    await db.execute('INSERT INTO notifications (user_id, message, type) VALUES (?, ?, ?)', [user_id, message, type])
-    return NextResponse.json({ success: true })
-}
--- my-app/src/app/confirmation/[type].js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Confirmation({ params }) {
-    return (
-        <div>
-            <h2>Confirmation: {params.type}</h2>
-        </div>
-    )
-}
